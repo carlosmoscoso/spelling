114fc1c019b31c7a9784fd8619257fc00e8c0689
Carlos Alexandre Moscoso <cmoscoso@MacBook-Air.local>
2018-10-13 16:53:45 -0300

    training may require the jvm to be started with a heap bigger than its default

--- project.clj
+++ project.clj
@@ -1,10 +1,4 @@
-(defproject spelling "0.1.0-SNAPSHOT"
-  :description "TODO"
-  :url "TODO"
-  :license {:name "TODO: Choose a license"
-            :url "http://choosealicense.com/"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.stuartsierra/component "0.3.2"]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
-                                  [com.stuartsierra/component.repl "0.2.0"]]
-                   :source-paths ["dev"]}})
+(defproject spelling "0.1.0"
+  :description "Simple implementation of spelling corrector."
+  :url "http://norvig.com/spell-correct.html"
+  :dependencies [[org.clojure/clojure "1.9.0"]])
